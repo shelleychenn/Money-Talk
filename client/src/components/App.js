f4dae501d28aa6a1b06d5bbe6b6638f2550acd86
shelleychenn <shelleychen322@gmail.com>
2020-06-18 22:14:34 -0400

    Implement update and delete functionalities

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,9 @@ class App extends React.Component {
     };
     this.getSavedEntries = this.getSavedEntries.bind(this);
     this.submitNewEntry = this.submitNewEntry.bind(this);
+    //this.updateEntry = this.updateEntry.bind(this);
+    this.update = this.update.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
   }
 
   componentDidMount() {
@@ -50,11 +53,73 @@ class App extends React.Component {
       });
   }
 
+  // updateEntry(newEntry) {
+  //   axios
+  //     .put('/budget', {
+  //       description: newEntry.description,
+  //       newDate: newEntry.newDate,
+  //       newDescription: newEntry.newDescription,
+  //       newAmount: newEntry.newAmount,
+  //       newTransactionType: newEntry.newTransactionType,
+  //       newCategory: newEntry.newCategory,
+  //       newAccountName: newEntry.newAccountName,
+  //     })
+  //     .then(() => {
+  //       console.log('entry updated successful');
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  update(entry, type) {
+    console.log(entry);
+    let entryPrompt;
+    if (type === 'description') {
+      entryPrompt = entry.description;
+    } else if (type === 'amount') {
+      entryPrompt = entry.amount;
+    }
+    let newValue = prompt(
+      `Please update your entry's ${type}`,
+      `${entryPrompt}`
+    );
+
+    if (newValue) {
+      entry[type] = newValue;
+      axios
+        .put(`/budget/${entry._id}`, entry)
+        .then(() => {
+          console.log('update successfully');
+          this.getSavedEntries();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  deleteEntry(entry) {
+    axios
+      .delete(`/budget/${entry._id}`)
+      .then(() => {
+        console.log('entry deleted!');
+        this.getSavedEntries();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Money Talk</h1>
-        <Summary entries={this.state.entries} />
+        <Summary
+          entries={this.state.entries}
+          update={this.update}
+          deleteEntry={this.deleteEntry}
+        />
         <Form submitNewEntry={this.submitNewEntry} />
       </div>
     );
