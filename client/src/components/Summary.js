519a5a2f97344a5932c39b147ee98ed6c8810673
Anna Makagonova <makagonova.ann@gmail.com>
2020-06-19 13:18:41 -0400

    Update Summary.js

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -6,22 +6,26 @@ const Summary = ({ entries, update, deleteEntry }) => {
     <div className="summary">
       <h2 className="summary-heading">This is where your money goes</h2>
       <table className="table">
-        <tr>
-          <th className="date">Date</th>
-          <th className="description">Description</th>
-          <th className="amount">Amount</th>
-          <th className="transactionType">Debit/Credit</th>
-          <th className="category">Category</th>
-          <th className="accountName">Account</th>
-        </tr>
-        {entries.map((entry) => (
-          <Entry
-            entry={entry}
-            key={entry._id}
-            update={update}
-            deleteEntry={deleteEntry}
-          />
-        ))}
+        <thead>
+          <tr>
+            <th className="date">Date</th>
+            <th className="description">Description</th>
+            <th className="amount">Amount</th>
+            <th className="transactionType">Debit/Credit</th>
+            <th className="category">Category</th>
+            <th className="accountName">Account</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry) => (
+            <Entry
+              entry={entry}
+              key={entry._id}
+              update={update}
+              deleteEntry={deleteEntry}
+            />
+          ))}
+        </tbody>
       </table>
     </div>
   );
